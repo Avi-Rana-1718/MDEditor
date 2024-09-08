f1d823a64495986190975d2db80baa46cdcda505
Avi Rana <itzavi1718@gmail.com>
2024-09-09 00:35:16 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import CodeEditor from "./components/CodeEditor"
+import Statement from "./components/Statement"
+
+function App() {
+  return (
+    <>
+    <div>
+    <CodeEditor/>
+    </div>
+    </>
+  )
+}
+
+export default App
